87b9f0659f6074c81fc02bfc6906fa346d24a717
mampiononavoahary <hei.zoarisoa@gmail.com>
2024-10-19 09:44:41 +0300

    feat: do service page

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -37,8 +37,30 @@ const services = [
 import { motion } from 'framer-motion'
 
 const Services = () => {
-  return<section>
-     <div className="container mx-auto">services page</div>
+  return<section className='min-h-[80vh] flex flex-col justify-center py-12 xl:py-0'>
+     <div className="container mx-auto">
+      <motion.div initial={{opacity:0}} animate={{
+        opacity:1,
+        transition:{ delay:2.4, duration:0.4, ease: "easeIn"},
+      }}
+      className='grid grid-cols-1 md:grid-cols-2 gap-[60px]'>
+        {services.map((service, index)=>{
+          return (
+            <div key={index} className='flex-1 flex flex-col justify-center gap-6 group'>
+              <div className='w-full flex justify-between items-center'>
+                <div className='text-5xl font-extrabold text-outline
+                text-transparent group-over:text-outline-hover'>{service.num}</div>
+                <Link href={service.href}></Link>
+                <BsArrowDownRight/>
+              </div>
+              <h2>{service.title}</h2>
+              <p>{service.description}</p>
+              <div className='border-b border-white/20 w-full'></div>
+            </div>
+          )
+        })}
+      </motion.div>
+     </div>
   </section>
 }
 
